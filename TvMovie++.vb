4a958ce13e76f409dfab6949d06976c3e3e33902
Scrounger@gmx.net <Scrounger@gmx.net>
2012-09-26 16:41:52 +0200

    new enrichEPG.dll Version added:  - renaming series to orginal series db name  - creating dummy schedule for Episodenscanner
    Signed-off-by: Scrounger@gmx.net <Scrounger@gmx.net>

--- TvMovie++.vb
+++ TvMovie++.vb
@@ -271,7 +271,7 @@ Namespace TvEngine
 
         Public ReadOnly Property Version() As String Implements ITvServerPlugin.Version
             Get
-                Return "1.3.2.5"
+                Return "1.3.3.0"
             End Get
         End Property
 
